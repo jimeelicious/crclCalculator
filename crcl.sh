8f4c995793bcc99aefc9fc43faf3d23b398e45ff
jimeelicious <xatsukex@gmail.com>
2018-03-20 10:52:07 -0700

    First commit

---
+++ crcl.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+debug=0
+showwork=1
+cutoffABWratio=1.2
+
+### Color settings ###
+GREEN='\033[7;32m'
+NC='\033[0m'
+
+### FUNCTIONS ###
+promptInput() {
+#prompt for height, weight, gender, and validates input
+read -p "Enter the age (years): " age
+#	 if ! $(echo $age | grep -Poq '^[0-9]\d*$'); then
+#	 echo Please enter the proper age.; exit 1
+#	 fi
+
+read -p "Enter the height (inches): " height
+#	 if ! $(echo $height | grep -Poq '^[0-9]\d*(\.\d+)?$'); then
+#	 echo Please enter the proper height in inches.; exit 1
+#	 fi
+
+read -p "Enter the weight (kilograms): " TBW
+#	 if ! $(echo $TBW | grep -Poq '^[0-9]\d*(\.\d+)?$'); then
+#	 echo Please enter the proper weight in kilograms.; exit 1
+#	 fi
+
+read -p "Indicate if male or female (m/f): " gender
+#	 if ! $(echo $gender | grep -Poq 'm|f'); then
+#	 echo Please enter either "m" or "f" only.; exit 1
+#	 fi
+
+read -p "Enter in the serum creatinine (mg/dL): " scr
+#	 if ! $(echo $scr | grep -Poq '^[0-9]\d*(\.\d+)?$'); then
+#	 echo Please enter the serum creatinine in mg/dL.; exit 1
+#	 fi
+}
+
+
+
+
+
+CalcIBW() {
+# If female, changes base weight at 60 inches to 45.5 kg
+if [ $gender = f ]; then
+   baseweight=45.5
+   else baseweight=50
+   fi
+
+# Determines how many inches above 60 inches, then adds 2.3 kgs for every inch above for IBW
+heightdiff=$(( $height - 60 ))
+weightdiffadj=$(python -c "print 2.3*$heightdiff")
+IBW=$(python -c "print $baseweight+$weightdiffadj")
+
+## DEBUG FUNCTION
+if [ $debug -eq 1 ]; then
+	echo
+	echo "Height diff in inches: $heightdiff"
+	echo "Weight Difference Adj, added on to ideal BW at 5 feet: $weightdiffadj"
+	fi
+
+# Spit out output
+if [ $showwork -eq 1 ]; then
+  echo "IBW is $baseweight($gender) + 2.3*(Height-60) = $baseweight($gender) + 2.3*($height-60) = $IBW"
+fi
+}
+
+
+
+
+DetWeightType() {
+# Determines if overweight or underweight.
+wtratio=$(python -c "print 100*$TBW/$IBW")
+wtratioRound=$(python -c "print int($wtratio)")
+cutoff=$(python -c "print int(100*$cutoffABWratio)")
+if [ $wtratioRound -ge "$cutoff" ]; then
+	ABWflag=1
+  elif [ $wtratioRound -lt 100 ]; then
+	skinnyFlag=1
+	echo Using total body weight because the patient\'s TBW/IBW ratio is $(python -c "print round($TBW/$IBW,2)") \< 1
+  fi
+
+## DEBUG FUNCTION
+if [ $debug -eq 1 ]; then
+	echo
+	echo "Weight ratio? $wtratio"
+	echo "Weight ratio Rounded? $wtratioRound"
+	echo "ABW flag set? $ABWflag"
+	echo "Underweight flag set? $skinnyFlag"
+	fi
+}
+
+
+
+GetABW(){
+# Calculates ABW. Finds difference between TBW, IBW first, then multiplies by 0.4 adjustment.
+TBWIBWdiff=$(python -c "print $TBW-$IBW")
+ABW=$(python -c "print 0.4*$TBWIBWdiff+$IBW")
+
+## DEBUG FUNCTION
+if [ $debug -eq 1 ]; then
+	echo
+	echo "TBW-IBW difference in kilograms: $TBWIBWdiff"
+	fi
+
+# Calculates final ABW result by adding above result
+if [ $showwork -eq 1 ]; then
+echo "ABW = IBW + 0.4*(TBW-IBW) = $IBW + 0.4*($TBW-$IBW) = $ABW"
+echo ABW is used because the patient\'s TBW/IBW ratio is $(python -c "print round($TBW/$IBW,2)") \> "${cutoffABWratio}".
+fi
+}
+
+
+
+CalculateCrCl() {
+# Sets either the adjusted, ideal, or total body weight to be used
+if [ "$ABWflag" = 1 ]; then
+	EqnWt=$ABW
+  elif [ "$skinnyFlag" = 1 ]; then
+	EqnWt=$TBW
+  else
+	EqnWt=$IBW
+  fi
+
+### CrCl Equation
+# Calculates the numerator
+num=$(python -c "print (140-$age)*$EqnWt")
+# Calculates the denominator
+den=$(python -c "print 72*$scr")
+# Divides
+CrCl=$(python -c "print $num/$den")
+# If female, multiply final result by 0.85
+if [ $gender = f ]; then
+	CrClFinal=$(python -c "print $CrCl*0.85")
+  else
+	CrClFinal=$CrCl
+  fi
+CrCl=$(python -c "print round($CrCl,1)")
+CrClFinal=$(python -c "print round($CrClFinal,1)")
+### End CrCl Equation
+
+# Spits out answer, showing work if requested.
+echo
+if [ $showwork -eq 1 ]; then
+	echo "CrCl = [(140-age)*weight] / (72*SCr) = [(140-${age})*${EqnWt}] / (72*${scr}) = $CrCl"
+	if [ $gender = f ]; then
+	  echo "Female patient, thus multiply by 0.85: (${CrCl})*(0.85) = $CrClFinal"
+	fi
+fi
+echo
+echo -e The patient\'s calculated creatine clearance is ${GREEN}$CrClFinal mL\/min${NC} \(gender\: $gender\).
+echo
+
+## DEBUG FUNCTION
+if [ $debug -eq 1 ]; then
+	echo
+	echo "Equation Weight used: $EqnWt"
+	echo "Equation NUMERATOR used: $num"
+	echo "Equation DEMOMINATOR used: $den"
+	fi
+}
+
+
+
+### SCRIPT ###
+echo "Serum creatine calculator v1.0 by Jimmy"
+echo -e "To quit, hit ${GREEN}control + C${NC}"
+echo "If the program is wonky (e.g. typing in a letter for an age),"
+echo "quit the application and rerun again."
+echo
+while true; do
+  promptInput
+  CalcIBW
+  DetWeightType
+  if [ "$ABWflag" = 1 ]; then
+	GetABW
+	fi
+  CalculateCrCl
+done
