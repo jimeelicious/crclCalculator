edca80f558b0dc05154cf234dd7c7f84604c2247
jimeelicious <xatsukex@gmail.com>
2018-03-27 16:07:10 -0700

    Add Windows 10 shell script

--- src/w10install.sh
+++ src/w10install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update &&
 sudo apt-get -y install python &&
 curl -L https://goo.gl/9tVe59 -o ~/crcl.sh &&
-chmod u+x ~/crcl.sh &&
+chmod 774 ~/crcl.sh &&
 
 grep "crcl.sh" ~/.bashrc
 if [ $? = 1 ]; then
