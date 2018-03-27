b923ffa7b598a6910fef00256dfbbe5b85faa534
jimeelicious <xatsukex@gmail.com>
2018-03-27 16:37:47 -0700

    Add Windows 10 shell script

--- src/w10install.sh
+++ src/w10install.sh
@@ -4,7 +4,7 @@ sudo echo "Installing! Please wait."
 sudo apt-get update &&
 sudo apt-get -y install python &&
 curl -L https://goo.gl/9tVe59 -o ~/crcl.sh &&
-chown "$USER":"$USER" ~/crcl.sh && sudo chmod 771 ~/crcl.sh
+chown "$USER":"$USER" ~/crcl.sh && chmod 771 ~/crcl.sh
 
 grep "crcl.sh" ~/.bashrc
 if [ $? = 1 ]; then
